59f55d153cda93f4e92630d57f3b80a938d0576d
dratech <alfonsotech@nym.hush.com>
2017-08-31 23:15:50 -0700

    Add scrapper into database

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var mongojs = require("mongojs");
+var request = require("request");
+var cheerio = require("cheerio");
 var app = express();
 
-app.use(express.static("public"));
-
-var databaseUrl = "zoo";
-var collections = ["animals"];
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
 var db = mongojs(databaseUrl, collections);
-
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
@@ -18,7 +17,8 @@ app.get("/", function(req, res) {
 });
 
 app.get("/all", function(req, res) {
-  db.animals.find({}, function(error, found) {
+
+  db.scrapedData.find({}, function(error, found) {
     if (error) {
       console.log(error);
     }
@@ -28,26 +28,31 @@ app.get("/all", function(req, res) {
   });
 });
 
-app.get("/name", function(req, res) {
-  db.animals.find().sort({ name: 1 }, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
+app.get("/scrape", function(req, res) {
+  request("https://news.ycombinator.com/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    $(".title").each(function(i, element) {
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
 
-app.get("/weight", function(req, res) {
-  db.animals.find().sort({ weight: -1 }, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
+      if (title && link) {
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(inserted);
+          }
+        });
+      }
+    });
   });
+
+  res.send("Scrape Complete");
 });
 
 app.listen(3000, function() {
